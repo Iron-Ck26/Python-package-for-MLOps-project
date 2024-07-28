468006efc8fb0e41bc9d7ba8dd996ac708656a1b
Iron_Ck26 <chakrapaniwaghmode81@gmail.com>
2024-07-28 20:04:41 +0530

    code updated4

--- src/mongodb_connect/mongo_crud.py
+++ src/mongodb_connect/mongo_crud.py
@@ -3,27 +3,27 @@ import os
 import pandas as pd
 from pymongo.mongo_client import MongoClient
 import json
-
+from ensure import ensure_annotations
 
 class mongo_operation:
     __collection = None  # Here I have created a private/protected variable
     __database = None
-
+    
     def __init__(self, client_url: str, database_name: str, collection_name: str = None):
         self.client_url = client_url
         self.database_name = database_name
         self.collection_name = collection_name
-
+       
     def create_mongo_client(self):
         client = MongoClient(self.client_url)
         return client
-
+    
     def create_database(self):
         if mongo_operation.__database is None:
             client = self.create_mongo_client()
             mongo_operation.__database = client[self.database_name]
         return mongo_operation.__database
-
+    
     def create_collection(self, collection_name=None):
         if collection_name is None:
             collection_name = self.collection_name
@@ -31,7 +31,7 @@ class mongo_operation:
             database = self.create_database()
             mongo_operation.__collection = database[collection_name]
         return mongo_operation.__collection
-
+    
     def insert_record(self, record: dict, collection_name: str = None) -> Any:
         collection = self.create_collection(collection_name)
         if isinstance(record, list):
@@ -44,20 +44,19 @@ class mongo_operation:
         else:
             raise TypeError("Record must be a dictionary or list of dictionaries")
         return result
-
+    
     def bulk_insert(self, datafile: str, collection_name: str = None):
         if not os.path.isfile(datafile):
             raise FileNotFoundError(f"No such file: '{datafile}'")
-
+        
         if datafile.endswith('.csv'):
             dataframe = pd.read_csv(datafile, encoding='utf-8')
         elif datafile.endswith(".xlsx"):
             dataframe = pd.read_excel(datafile, encoding='utf-8')
         else:
             raise ValueError("Unsupported file format. Please provide a '.csv' or '.xlsx' file.")
-
+            
         datajson = json.loads(dataframe.to_json(orient='records'))
         collection = self.create_collection(collection_name)
         result = collection.insert_many(datajson)
         return result
-
