37686b36c872202f4bc35d48a787ff5d6f8bf7d6
Iron_Ck26 <chakrapaniwaghmode81@gmail.com>
2024-07-28 06:36:27 +0530

    code updated5

--- tests/unit/test_mongo_operation.py
+++ tests/unit/test_mongo_operation.py
@@ -1,12 +1,26 @@
 import pytest
 from mongodb_connect.mongo_crud import MongoOperation
 
-# It's better to load these from environment variables or a secure config file
-client_url = "mongodb+srv://Iron_Ck:55252555_Ck@mlops-mongodb-cluster1.etxtatl.mongodb.net/?retryWrites=true&w=majority&appName=MLOps-MongoDB-Cluster1"
-database = "mynewdatabase"
-collection_name = "mynewcolumn"
+def test_mongo_operation_init():
+    client_url = "mongodb+srv://Iron_Ck:55252555_Ck@mlops-mongodb-cluster1.etxtatl.mongodb.net/?retryWrites=true&w=majority&appName=MLOps-MongoDB-Cluster1"
+    database_name = "mynewdatabase1"
+    collection_name = "mynewcolumn1"
+    
+    mo = MongoOperation(client_url, database_name, collection_name)
+    
+    assert mo.client_url == client_url
+    assert mo.database_name == database_name
+    assert mo.collection_name == collection_name
 
-# Create an instance of MongoOperation
-mongo_op = MongoOperation(client_url, database, collection_name)
+def test_create_mongo_client():
+    client_url = "mongodb+srv://Iron_Ck:55252555_Ck@mlops-mongodb-cluster1.etxtatl.mongodb.net/?retryWrites=true&w=majority&appName=MLOps-MongoDB-Cluster1"
+    database_name = "mynewdatabase1"
+    collection_name = "mynewcolumn1"
+    
+    mongo_op = MongoOperation(client_url, database_name, collection_name)
+    client = mongo_op.create_mongo_client()
+    
+    assert client is not None
+    # You might want to add more specific assertions about the client
 
-# Now you can use mongo_op to perform operations on your MongoDB
+# Add more tests as needed
